6784817bdd08104aee3e3701fa1a845b83bbb107
tkt-gemini <tkt310505@gmail.com>
2026-05-19 17:25:47 +0000

    feat: change treesitter config

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -4,7 +4,7 @@
 -- local autocmd = vim.api.nvim_create_autocmd
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'rust', 'javascript', 'zig', 'python', 'lua', 'markdown', 'quarto' },
+  -- pattern = { 'rust', 'javascript', 'zig', 'python', 'lua', 'markdown', 'quarto' },
   callback = function()
     vim.treesitter.start()
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,22 +5,12 @@ return {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
     build = ":TSUpdate",
-    config = function()
-      local ts = require("nvim-treesitter")
-      local languages = { "python", "markdown", "markdown_inline", "lua", "vim", "bash", "c", "cpp" }
-      local installed = ts.get_installed()
-      local installing = {}
-
-      for _, lang in ipairs(languages) do
-        if not table.contains(installed, lang) then
-          table.insert(installing, lang)
-        end
-      end
-
-      if #installing > 0 then
-        ts.install(installing)
-      end
-
+    opts = {
+      ensure_installed = { "python", "markdown", "markdown_inline", "lua", "vim", "bash", "c", "cpp" },
+      auto_install = true,
+    },
+    config = function(_, opts)
+      require("nvim-treesitter").setup(opts)
       vim.treesitter.language.register("markdown", "quarto")
     end,
   }
