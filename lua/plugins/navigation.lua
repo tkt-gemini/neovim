07b9671019a2ab0e36b6cc4b0a1b73b81a7a4b25
tkt-gemini <tkt310505@gmail.com>
2026-06-18 05:17:45 +0000

    feat: add new shotcut for find all files (hidden file)

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -33,6 +33,7 @@ return {
 					"%.lock",
 					"%.git/",
 					"%.pixi/",
+          "%.cache/",
 					"build/",
 					"dist/",
 					"vendor/",
@@ -41,15 +42,15 @@ return {
           "%.o",
           "%.out",
           "%.exe",
-          "%.cache/",
 				},
 			},
 		},
     keys = {
-      { "<leader>ff", "<CMD>Telescope find_files hidden=true<CR>", desc = "Find Files" },
-      { "<leader>fg", "<CMD>Telescope live_grep<CR>", desc = "Live Grep" },
-      { "<leader>fb", "<CMD>Telescope buffers<CR>", desc = "Find Buffers" },
-      { "<leader>fh", "<CMD>Telescope help_tags<CR>", desc = "Help Tags" },
+      { "<leader>ff", "<CMD>Telescope find_files<CR>", desc = "find files" },
+      { "<leader>fi", "<CMD>Telescope find_files hidden=true<CR>", desc = "find all files" },
+      { "<leader>fg", "<CMD>Telescope live_grep<CR>", desc = "live grep" },
+      { "<leader>fb", "<CMD>Telescope buffers<CR>", desc = "find buffers" },
+      { "<leader>fh", "<CMD>Telescope help_tags<CR>", desc = "help tags" },
     },
 		config = function(_, opts)
 			local telescope = require('telescope')
